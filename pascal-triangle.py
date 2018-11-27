b204b50f847e73943b1f3f5215b4916c2fc94a1b
Krzysztof B <hnic85@gmail.com>
2018-11-27 17:23:02 +0100

    Create pascal-triangle.py

---
+++ pascal-triangle.py
@@ -0,0 +1,42 @@
+'''
+Pascal triangle - and proper code to print it.
+
+Written: 12.11.2018
+Author: Chris Brymer
+'''
+
+
+width = 50
+height = 10
+
+numbers = [1]
+
+line = ''
+
+for n in numbers:
+
+    line += '%3d ' % (n)
+
+print(line.center(width))
+
+
+for i in range(height-1):
+
+    newNumbers = [1]
+
+    position = 0
+
+    while position < len(numbers)-1:
+
+        newNumbers.append(numbers[position] + numbers[position+1])
+        position += 1
+
+    newNumbers.append(1)
+    numbers = newNumbers.copy()
+
+    line = ''
+    for n in numbers:
+
+        line += '%3d ' % (n)
+
+    print(line.center(width))
